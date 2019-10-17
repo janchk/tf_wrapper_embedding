6cc7933bd95fe2d6690b76cbb148a59e98c212ac
Jan Akhremchik <janchk@gmail.com>
2019-10-18 01:56:23 +0300

    init commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+// #include <ten>
+#include "tensorflow/core/public/session.h"
+
+//#include "tensorflow/core/public/session.h"
+//#include "tensorflow/core/graph/default_device.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
