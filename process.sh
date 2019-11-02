144fe60cc6252a18e22692c1334682318d085a65
Jan Akhremchik <janchk@gmail.com>
2019-11-02 17:02:46 +0300

    Such a stable version. So working. So WoW.

--- process.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cmake-build-debug/TF_WRAPPER_EMBEDDING \
-                  -img /home/jakhremchik/Downloads/lena-256x256.jpg \
-                  -pb /home/jakhremchik/Downloads/optimized_model_output.pb \
-                  --input_node input:0
