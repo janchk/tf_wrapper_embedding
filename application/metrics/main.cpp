de35c7769bb0c5299d88421ce482389df4d0f073
Jan Akhremchik <janchk@gmail.com>
2019-11-08 20:33:00 +0300

    Small changes

--- application/metrics/main.cpp
+++ application/metrics/main.cpp
@@ -1,4 +1,9 @@
 //
 // Created by jakhremchik
 //
+#include "metrics_base.h"
 
+int main(int argc, char *argv[]) {
+    auto *tf_wrapper = new MetricsBase();
+    std::cout << tf_wrapper->topN << std::endl;
+}
