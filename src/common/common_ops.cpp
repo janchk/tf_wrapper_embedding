6e31603de43f4ad37fd4143d77c9beba5c54b2bd
Jan Akhremchik <janchk@gmail.com>
2019-11-13 21:34:42 +0300

    not the best solution but it works.

--- src/common/common_ops.cpp
+++ src/common/common_ops.cpp
@@ -5,38 +5,39 @@
 #include "common_ops.h"
 
 std::string common_ops::extract_class(const std::string &filepath) {
-   std::string slash_delim = "/"; //TODO another delimiter for windows
+   std::string spilt_delim = "/"; //TODO another delimiter for windows
    std::string token;
    std::string train_identifier = "series/";
    std::string test_identifier = "queries/";
-   std::string classname_delim = "__"; //need to change __ -> /
+   std::string classname_delim = "__";
+   size_t pos_end;
 
    size_t pos_begin = filepath.find(test_identifier);
-   size_t pos_end = filepath.find(classname_delim);
+
 
    if (std::string::npos == pos_begin) { // if not test directory
-       pos_begin = filepath.find(train_identifier); // find train directory identifier
-       if (std::string::npos == pos_end) {
-           classname_delim = "/";
-           token = filepath.substr(pos_begin + train_identifier.size(), std::string::npos);
-           pos_end = token.find(classname_delim);
+       pos_begin = filepath.find(train_identifier) + train_identifier.size(); // find train directory identifier
+       token = filepath.substr(pos_begin, std::string::npos);
+       pos_end = token.find(spilt_delim);
+
+       token = token.substr(0, pos_end);
+
+       pos_end = token.find(classname_delim);
+       if (std::string::npos != pos_end) {
            token = token.substr(0, pos_end);
-       } else {
-           pos_end = filepath.find(classname_delim);
-           pos_begin = pos_begin + train_identifier.size();
-           token = filepath.substr(pos_begin, pos_end - pos_begin);
        }
 
-   } else {
-       if (std::string::npos == pos_end) {
-           classname_delim = "/";
-           token = filepath.substr(pos_begin + test_identifier.size(), std::string::npos);
-           pos_end = token.find(classname_delim);
+
+   } else { //we assume that we are in test directory
+       pos_begin = filepath.find(test_identifier) + test_identifier.size();
+       token = filepath.substr(pos_begin, std::string::npos);
+       pos_end = token.find(spilt_delim);
+
+       token = token.substr(0, pos_end);
+
+       pos_end = token.find(classname_delim);
+       if (std::string::npos != pos_end) {
            token = token.substr(0, pos_end);
-       } else {
-           pos_end = filepath.find(classname_delim);
-           pos_begin = pos_begin + test_identifier.size();
-           token = filepath.substr(pos_begin, pos_end - pos_begin);
        }
    }
    return token;
