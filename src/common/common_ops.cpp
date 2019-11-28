c17e58fcf5df44a6613c792b88ab1173796bbaf7
Jan Akhremchik <janchk@gmail.com>
2019-11-28 20:29:54 +0300

    little cleaning

--- src/common/common_ops.cpp
+++ src/common/common_ops.cpp
@@ -48,37 +48,3 @@ std::string common_ops::extract_class(const std::string &filepath) {
    return token;
 }
 
-std::vector<std::string> common_ops::split(const std::string &text, const std::string &delim) {
-    std::vector<std::string> results;
-    for (auto p = cbegin(text); p != cend(text); ) {
-        const auto n = search(p, cend(text), cbegin(delim), cend(delim));
-        results.emplace_back(p, n);
-        p = n;
-        if (cend(text) != n) // we found delim, skip over it.
-            p += delim.length();
-    }
-    return results;
-}
-
-void common_ops::check_end_slash(std::string &path) {
-    if (path[path.size() - 1] == '/')
-        path.pop_back();
-}
-
-std::string common_ops::extract_class(const std::string &path_to_img, std::string &series_path, std::string &queries_path) {
-    size_t class_pos = 0;
-    check_end_slash(series_path);
-    check_end_slash(queries_path);
-    size_t in_queries_pos = path_to_img.find(queries_path);
-    size_t in_imgs_pos = path_to_img.find(series_path);
-
-    if (in_queries_pos == std::string::npos)
-        class_pos = in_imgs_pos + series_path.size() + 1;
-    else
-        class_pos = in_queries_pos + queries_path.size() + 1;
-
-    std::string relative_img_path = path_to_img.substr(class_pos, std::string::npos);
-    std::string classname = split(relative_img_path, "/")[0];
-    classname = split(classname, "__")[0];
-    return classname;
-}
